ed789892027f6f97d93ce74d887778631ef27989
Travis <travis.cox@unimelb.edu.au>
2016-06-18 16:04:54 +1000

    Playing with hinges and ropes

---
+++ Unity/Assets/ChainOrganiser.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainOrganiser : MonoBehaviour {
+
+	public float Spring = 10;
+	public float Damper = 0;
+	public float TargetPosition = 1;
+
+	public GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		HingeJoint[] joints;
+		joints = parent.GetComponentsInChildren<HingeJoint> ();
+		Rigidbody[] bodies;
+		bodies = parent.GetComponentsInChildren<Rigidbody> ();
+
+		foreach (HingeJoint j in joints) {
+
+			j.useSpring = false;
+			j.useLimits = true;
+
+//			JointSpring n = new JointSpring();
+//			n.spring = Spring;
+//			n.damper = Damper;
+//			n.targetPosition = TargetPosition;
+//			j.spring = n;
+
+//			j.spring.spring = Spring;
+//			j.spring.damper = Damper;
+//			j.spring.targetPosition = TargetPosition;
+		}
+
+		foreach (Rigidbody r in bodies) {
+			//r.useGravity = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public float speed;
+	public Vector3 axis;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (axis, speed);
+	}
+}
