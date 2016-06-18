40a717af0ad5d2899f087540c169f4f541145ec1
Travis <travis.cox@unimelb.edu.au>
2016-06-19 09:52:33 +1000

    Added more motion

---
+++ Unity/Assets/BounceObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceObject : MonoBehaviour {
+
+	public bool Bounce = false;
+	public float NoiseSpeed = 0.01f;
+	public float NoiseStrength = 10.0f;
+
+	private float noiseValue = 0;
+	private float noiseTime = 0;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Bounce) {
+			if (Random.Range (0.0f, 1.0f) < 0.2f) {
+				noiseTime += NoiseSpeed;
+				noiseValue += NoiseSpeed;
+				float t = Mathf.PerlinNoise (noiseValue, noiseTime);
+				t *= NoiseStrength;
+
+				if (transform.position.y < 4){
+					GetComponent<Rigidbody> ().AddForce (new Vector3 (0, t, 0));
+				}
+			}
+		}
+	}
+}
---
+++ Unity/Assets/SpotlightLookAtObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpotlightLookAtObject : MonoBehaviour {
+
+	public Transform TargetObject;
+
+	void Update () {
+		transform.LookAt (TargetObject);
+	}
+}
