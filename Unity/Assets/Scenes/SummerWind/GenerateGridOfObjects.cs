9dcd74a23cb206f913962128806dc1ebe2308e88
Travis <travis@denouncetheclock.com>
2016-08-15 07:34:52 +1000

    New works

---
+++ Unity/Assets/Scenes/SummerWind/GenerateGridOfObjects.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateGridOfObjects : MonoBehaviour {
+
+	public GameObject ObjectToConstruct;
+
+	public int ObjectXNumber = 10;
+	public int ObjectYNumber = 10;
+
+	public float SpacingX = 6.5f;
+	public float SpacingY = 6.0f;
+
+	public bool OffsetRows = true;
+
+	private IList<GameObject> gridObjects;
+
+	void Start () {
+		gridObjects = new List<GameObject> ();
+		for (int x = 0; x < ObjectXNumber; x++) {
+			for (int y = 0; y < ObjectYNumber; y++) {
+				Vector3 pos = new Vector3 (x * SpacingX,0, y * SpacingY);
+				if (OffsetRows) {
+					if (y % 2 == 0) {
+						pos.x += (SpacingX / 2.0f);
+					}
+				}
+				GameObject g = Instantiate (ObjectToConstruct, pos, Quaternion.identity) as GameObject;
+				gridObjects.Add (g);
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Scenes/SummerWind/TranslateOnAxisOverTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslateOnAxisOverTime : MonoBehaviour {
+
+	public Vector3 TranslationAxis;
+
+	public Vector2 NoiseSpeed = new Vector2(0.01f, 0.01f);
+
+	private Vector2 noiseBase = new Vector2(0,0);
+
+	private Vector3 basePosition;
+
+	void Start(){
+		basePosition = transform.position;
+	}
+
+	void Update () {
+		float noise = (Mathf.PerlinNoise (noiseBase.x, noiseBase.y) - 0.5f)*2;
+		transform.position = basePosition + (TranslationAxis * noise);
+		noiseBase += NoiseSpeed;
+	}
+}
